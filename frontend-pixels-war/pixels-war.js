c6ef8a1836b1944efa1607c75208fa15602713cb
Louis Cristante <louis.cristante@etu.minesparis.psl.eu>
2026-04-29 20:39:56 +0200

    Pixel War answers

--- frontend-pixels-war/pixels-war.js
+++ frontend-pixels-war/pixels-war.js
@@ -9,7 +9,7 @@
 document.addEventListener("DOMContentLoaded",
     async () => {
 
-        let MAP_ID = "TEST"
+        let MAP_ID = undefined
         let API_KEY = undefined
 
         // for starters we get the list of maps from the server
@@ -47,12 +47,25 @@ document.addEventListener("DOMContentLoaded",
         async function connect(event) {
             //TODO:
             // - retrieves the selected map id (from the dropdown)
+            MAP_ID = document.getElementById("mapid-input").value
             // - sends the /init request to the server for this map id
+            console.log(`Connecting to map ${MAP_ID}...`)
+            const map_init_response = await fetch(`/api/v2/${MAP_ID}/init`) 
+            const map_init_json = await map_init_response.json()
             // - check the response status code as usual
+            if (!map_init_response.ok) {
+                alert(`Error initializing map ${MAP_ID}: ${map_init_response.status} ${map_init_response.statusText}`)
+                return
+            }
             // - initialize the map when OK
+            console.log(map_init_json)
+            API_KEY = map_init_json.api_key
+            console.log(`Connected to map ${MAP_ID}, on api key ${API_KEY}`)
+            draw_map(map_init_json.ni, map_init_json.nj, map_init_json.data)
         }
 
         //TODO: and attach it to the Connect button
+        document.getElementById("connect-button").addEventListener("click", connect)
 
         //TODO:
         // write a function that draws a map inside the griv div
@@ -64,28 +77,41 @@ document.addEventListener("DOMContentLoaded",
         // also give the child div's the 'pixel' class to leverage the default css
         // also don't forget to set the gridTemplateColumns of the grid div
         function draw_map(ni, nj, data) {
+            const grid = document.getElementById("grid")
+            //empty grid
+            grid.innerHTML = ""
 
+            grid.style.gridTemplateColumns = `repeat(${nj}, 1fr)`
+            for (let i = 0; i < ni; i++) {
+                for (let j = 0; j < nj; j++) {
+                    const pixel = document.createElement("div")
+                    pixel.classList.add("pixel")
+
+                    pixel.addEventListener("click", () => {
+                        console.log("Clicked pixel:", i, j)
+                        set_pixel(i, j)
+                    })
+
+                    const [r, g, b] = data[i][j]
+                    pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+                    grid.appendChild(pixel)
+                }
+            }
         }
-
-        //TMP: to test the previous function: 3 lines and 5 columns
-        draw_map(3, 5, [
-            [ [255, 0, 0], [255, 255, 0], [255, 0, 0], [255, 255, 0], [255, 0, 0] ],
-            [ [255, 255, 0], [255, 0, 0], [255, 255, 0], [255, 0, 0], [255, 255, 0] ],
-            [ [255, 0, 0], [255, 255, 0], [255, 0, 0], [255, 255, 0], [255, 0, 0] ],
-        ])
 
         //TODO:
         // write a function that applies a set of color changes
         // the input is a collection of 5-tuples of the form i, j, r, g, b
         function apply_changes(ni, nj, changes) {
+            const grid = document.getElementById("grid")
+            for (const [i, j, r, g, b] of changes) {
+                //pixels are added row by row
+                const index = i * nj + j
+                const pixel = grid.children[index]
+                pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+            }
         }
-
-        //TMP: to test the previous function, let's change the color of 3 pixels
-        apply_changes(3, 5,[
-            [1, 1, 0, 0, 255],
-            [1, 2, 0, 0, 255],
-            [1, 3, 0, 0, 255],
-        ])
 
         //TODO:
         // now that we have the JSON data that describes the map, we can
@@ -94,8 +120,30 @@ document.addEventListener("DOMContentLoaded",
         //TODO:
         // now that we have the API-KEY,
         // write a refresh(...) function that updates the grid
+        async function refresh() {
+            
+            const refresh_response = await fetch(`/api/v2/${MAP_ID}/deltas`, {
+                method: "GET",
+                headers : {
+                    'map_name': MAP_ID,
+                    'API-KEY': API_KEY,
+                },
+                credentials: "include",
+            })
+            const refresh_json = await refresh_response.json()
+            if (!refresh_response.ok) {
+                alert(`Error refreshing map ${MAP_ID}: ${refresh_response.status} ${refresh_response.statusText}`)
+                return
+            }
+            console.log(refresh_json)
+            console.log(`Refreshing map ${MAP_ID}...`)
+            apply_changes(refresh_json.ni, refresh_json.nj, refresh_json.deltas)
+            console.log(`Map ${MAP_ID} refreshed successfully`)
+        }
         // and attach this function to the refresh button click
-
+        document.getElementById("refresh-button").addEventListener("click", refresh)
+        
+        
         //TODO:
         // to be able to color a pixel: write a set_pixel(...)
         // function that sends a request to the server to color a pixel
@@ -103,9 +151,51 @@ document.addEventListener("DOMContentLoaded",
         // the color is taken from the color picker (code provided below)
         // it's up to you to find a way to get the pixel coordinates
 
+        async function set_pixel(i, j) {
+            console.log("Sending pixel:", i, j, getPickedColorInRGB())
+            const [r, g, b] = getPickedColorInRGB()
+            const set_pixel_response = await fetch(`/api/v2/${MAP_ID}/set`, {
+                method: "POST",
+                headers: {
+                    'map_name': MAP_ID,
+                    'API-KEY': API_KEY,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    'i': i,
+                    'j': j,
+                    'r': r,
+                    'g': g,
+                    'b': b
+                }),
+                credentials: "include",
+            })
+
+            const result = JSON.parse(await set_pixel_response.text())
+            console.log("Set response:", result)
+
+            if (typeof result === "number" && result > 0) {
+                const seconds = result / 1_000_000_000
+                alert(`Wait ${seconds.toFixed(1)} seconds before placing another pixel.`)
+                return
+            }
+
+            if (!set_pixel_response.ok) {
+                alert(`Error setting pixel: ${set_pixel_response.status} ${set_pixel_response.statusText}`)
+                return
+            }
+
+            if (typeof result === "number" && result === 0){
+                console.log(`Pixel set successfully: ${i}, ${j}`)
+                refresh()
+            }
+        }
+
         //TODO:
         // why not refresh the grid every 2 seconds?
         // or even refresh the grid after clicking a pixel?
+        connect() // connect to the first map by default
+        setInterval(refresh, 2000) // refresh every 2 seconds
 
         // ---- cosmetic / convenience / bonus:
 
